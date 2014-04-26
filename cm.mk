adcf53b94e32667e5336c894b91f41b683a175da
raw99 <raw9965@gmail.com>
2014-04-27 00:52:12 +0300

    crazy stuff

--- cm.mk
+++ cm.mk
@@ -7,16 +7,14 @@ $(call inherit-product, device/samsung/torino/full_torino.mk)
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
-# Include GSM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-# Release name and versioning
-PRODUCT_RELEASE_NAME := Galaxytorino
-PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S6102
-
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := torino
 PRODUCT_NAME := cm_torino
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-S6102
+
+# Set build fingerprint / ID / Product Name ect.
+#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-S6102 TARGET_DEVICE=GT-S6102 BUILD_FINGERPRINT=samsung/GT-S6102/GT-S6102:2.3.6/GINGERBREAD/DDLG1:user/release-keys PRIVATE_BUILD_DESC="GT-S6102-user 2.3.6 GINGERBREAD DDLG1 release-keys"
